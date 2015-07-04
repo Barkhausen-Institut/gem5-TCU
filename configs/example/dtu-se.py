5acdeac109876a1824117ef1f26fbef70e423b57
Christian Menard <christian.menard@gmx.de>
2015-07-04 15:37:34 +0200

    DTU: allow for multiple workloads by creating a system for every PE

--- configs/example/dtu-se.py
+++ configs/example/dtu-se.py
@@ -55,8 +55,8 @@ parser.add_option("--cpu-type", type="choice", default="atomic",
                   choices=CpuConfig.cpu_names(),
                   help = "type of cpu to run with")
 
-parser.add_option("-c", "--cmd", default="",
-                  help="The binary to run in syscall emulation mode on PE0.")
+parser.add_option("-c", "--cmd", default="", type="string",
+                  help="comma separated list of binaries")
 
 parser.add_option("--list-mem-types",
                   action="callback", callback=_listMemTypes,
@@ -109,31 +109,55 @@ CPUClass = CpuConfig.get(options.cpu_type)
 if CPUClass.require_caches():
     fatal("We don't want caches but %s must be used with caches" % options.cpu_type)
 
+# Each PE is represented as an instance of System. Wherea each PE has a CPU,
+# a Scratchpad, and a DTU connected via a crossbar. The PEs are connected via
+# a global crossbar.
+
+###############################################################################
+# root                                                                        #
+#                                                                             #
+# |-----------------|    |-----------------|    |-----------------|           #
+# | pe0             |    | pe1             |    | pe2             |           #
+# |         cpu     |    |         cpu     |    |         cpu     |           #
+# |          |      |    |          |      |    |          |      |           #
+# |        xbar     |    |        xbar     |    |        xbar     |           #
+# |       /    \    |    |       /    \    |    |       /    \    |           #
+# | scratchpad  dtu |    | scratchpad  dtu |    | scratchpad  dtu |           #
+# |              |  |    |              |  |    |              |  |           #
+# |--------------+--|    |--------------+--|    |--------------+--|           #
+#                |                      |                      |              #
+#                |                      |                      |              #
+#          noc --O----------------------O----------------------O----          #
+#                                                                             #
+###############################################################################
+
 # Set up the system
-system = System(mem_mode = CPUClass.memory_mode())
+root = Root(full_system = False)
 
 # Create a top-level voltage domain
-system.voltage_domain = VoltageDomain(voltage = options.sys_voltage)
+root.voltage_domain = VoltageDomain(voltage = options.sys_voltage)
 
 # Create a source clock for the system and set the clock period
-system.clk_domain = SrcClockDomain(clock =  options.sys_clock,
-                                   voltage_domain = system.voltage_domain)
+root.clk_domain = SrcClockDomain(clock =  options.sys_clock,
+                                 voltage_domain = root.voltage_domain)
 
 # Create a CPU voltage domain
-system.cpu_voltage_domain = VoltageDomain()
+root.cpu_voltage_domain = VoltageDomain()
 
 # Create a separate clock domain for the CPUs
-system.cpu_clk_domain = SrcClockDomain(clock = options.cpu_clock,
-                                       voltage_domain = system.cpu_voltage_domain)
+root.cpu_clk_domain = SrcClockDomain(clock = options.cpu_clock,
+                                     voltage_domain = root.cpu_voltage_domain)
 
 # All PEs are connected to a NoC (Network on Chip). In this case it's just
 # a simple XBar.
-system.noc = NoncoherentXBar(forward_latency  = 0,
-                             frontend_latency = 1,
-                             response_latency = 1,
-                             width = 8,
-                             )
+root.noc = NoncoherentXBar(forward_latency  = 0,
+                           frontend_latency = 1,
+                           response_latency = 1,
+                           width = 8,
+                          )
 
+cmd_list = options.cmd.split("," )
+workload_list = []
 
 # A PE (processing element) consists of a CPU, a Scratchpad-Memory, and a DTU.
 # These elements are connected via a simple non-coherent XBar. The DTU ports
@@ -142,8 +166,8 @@ system.noc = NoncoherentXBar(forward_latency  = 0,
 for i in range(0, options.num_pes):
 
     # each PE is represented by it's own subsystem
-    pe = SubSystem()
-    setattr(system, 'pe%d' % i, pe)
+    pe = System(mem_mode = CPUClass.memory_mode())
+    setattr(root, 'pe%d' % i, pe)
 
     # TODO set latencies
     pe.xbar = NoncoherentXBar(forward_latency  = 0,
@@ -152,7 +176,7 @@ for i in range(0, options.num_pes):
                               width = 8,
                              )
 
-    pe.scratchpad = Scratchpad()
+    pe.scratchpad = Scratchpad(in_addr_map = "true")
     pe.scratchpad.port = pe.xbar.master
 
     pe.dtu = Dtu()
@@ -160,67 +184,37 @@ for i in range(0, options.num_pes):
     pe.dtu.scratchpad = pe.xbar.slave
     pe.dtu.cpu = pe.xbar.master
 
-    pe.dtu.noc_master = system.noc.slave
-    pe.dtu.noc_slave  = system.noc.master
+    pe.dtu.noc_master = root.noc.slave
+    pe.dtu.noc_slave  = root.noc.master
 
-    # we only create only CPU as we only have one system port and therfore
-    # cannot load programs to multiple memories
-    if i == 0:
-        pe.cpu = CPUClass(cpu_id = i)
-        pe.cpu.clk_domain = system.cpu_clk_domain
+    pe.system_port = pe.xbar.slave
+
+    if i < len(cmd_list):
+        pe.cpu = CPUClass()
+        pe.cpu.clk_domain = root.cpu_clk_domain
 
         process = LiveProcess()
         process.cwd = os.getcwd()
 
-        process.executable = options.cmd
-        system.system_port = pe.xbar.slave
+        process.executable = cmd_list[i]
 
         process.cmd = process.executable
+        #process.use_init_port = 'true'
+        #process.init_port = pe.xbar.slave
 
         pe.cpu.workload = process;
 
         pe.cpu.createInterruptController()
         pe.cpu.connectAllPorts(pe.xbar)
 
-# setup Memory Controllers (from MemConfig.config_mem)
-
-nbr_mem_ctrls = options.mem_channels
-intlv_bits = int(math.log(nbr_mem_ctrls, 2))
-if 2 ** intlv_bits != nbr_mem_ctrls:
-    fatal("Number of memory channels must be a power of 2")
-
-cls = MemConfig.get(options.mem_type)
-mem_ctrls = []
-
-# We don't have chaches so we set to fixed 128 Byte granularity
-intlv_size = 128
-
-for i in xrange(nbr_mem_ctrls):
-    mem_ctrl = MemConfig.create_mem_ctrl(cls,
-                                         AddrRange(options.mem_size),
-                                         i,
-                                         nbr_mem_ctrls,
-                                         intlv_bits,
-                                         intlv_size)
-
-    # Set the number of ranks based on the command-line
-    # options if it was explicitly set
-    if issubclass(cls, m5.objects.DRAMCtrl) and \
-            options.mem_ranks:
-        mem_ctrl.ranks_per_channel = options.mem_ranks
-
-    mem_ctrl.port = system.noc.master
-
-    mem_ctrls.append(mem_ctrl)
-
-system.mem_ctrls = mem_ctrls
-
-root = Root(full_system = False, system = system)
+        workload_list.append(pe.cpu.workload)
 
 # Instantiate configuration
 m5.instantiate()
 
-system.pe0.cpu.workload[0].map(0x1000000, 0x1000000, 1024)
+# map the DTU registers
+for workload in workload_list:
+    workload[0].map(0x1000000, 0x1000000, 1024)
 
 # Simulate until program terminates
 exit_event = m5.simulate(options.maxtick)
