3afb1399c4fe0c4deaaadb45791f2a6262bbff65
Nils Asmussen <nils@os.inf.tu-dresden.de>
2016-09-06 13:02:55 +0200

    DTU: abort PFs, but leave them enabled on abort.

--- src/mem/dtu/dtu.cc
+++ src/mem/dtu/dtu.cc
@@ -351,9 +351,8 @@ Dtu::abortCommand()
 
     if (abort & Command::ABORT_VPE)
     {
-        DPRINTF(DtuCmd, "Resetting VPE id and disabling PFs\n");
+        DPRINTF(DtuCmd, "Resetting VPE id and aborting PFs\n");
         regs().set(DtuReg::VPE_ID, INVALID_VPE_ID);
-        regs().set(DtuReg::FEATURES, 0);
         if (ptUnit)
             ptUnit->abortAll();
     }
