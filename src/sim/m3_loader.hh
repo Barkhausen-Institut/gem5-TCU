53db9a8dd8ce8b52060a109ae225c084938dbbf8
Nils Asmussen <nils@script-solution.de>
2017-12-19 16:17:40 +0100

    DTU: increased stack size.
    Change-Id: I1b75249628875a3142f66351bed2c32d89fa8d9c

--- src/sim/m3_loader.hh
+++ src/sim/m3_loader.hh
@@ -44,7 +44,7 @@ class M3Loader
     static const size_t MAX_PES         = 64;
     static const size_t RT_SIZE         = 0x2000;
     static const uintptr_t RT_START     = 0x6000;
-    static const size_t STACK_SIZE      = 0x1000;
+    static const size_t STACK_SIZE      = 0x4000;
     static const uintptr_t STACK_AREA   = RT_START + RT_SIZE;
     static const size_t HEAP_SIZE       = 0x1000;
 
