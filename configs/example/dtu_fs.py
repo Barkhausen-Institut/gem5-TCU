eb3658fabd82a7331bdecf68880f466f9346a704
Nils Asmussen <nils@os.inf.tu-dresden.de>
2018-03-17 15:01:20 +0100

    DTU: increased endpoint count to 16.

--- configs/example/dtu_fs.py
+++ configs/example/dtu_fs.py
@@ -218,7 +218,7 @@ def createPE(noc, options, no, systemType, l1size, l2size, spmsize, dtupos, memP
     pe.dtu.core_id = no
 
     pe.dtu.coherent = options.coherent
-    pe.dtu.num_endpoints = 12
+    pe.dtu.num_endpoints = 16
     if dtupos > 0:
         pe.dtu.tlb_entries = 32
     else:
