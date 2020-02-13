8fa9ec6e50122c8ebc839f8943a78410f651a2f9
Nils Asmussen <nils.asmussen@barkhauseninstitut.org>
2020-02-13 14:25:54 +0100

    arch-riscv,tests: improved run-tests.py.
    This commits improves a couple of things in the run-tests.py script that
    executes the RISC-V test suite:
    
    - clusterjob is removed as it was not used
    - a single test can be run now by specifying the name via --test
    - the CPU model and debug flags can be specified via argument
    - the p and v tests are now supported
    - the tests can now be aborted via ^C
    - the output is written to a per-test file, so that details about each
      test can be inspected afterwards.
    - a progress is shown
    
    Change-Id: Iab1d8d4021a3dfcf62da5c7839258b28cdf99eac

--- tests/test-progs/asmtest/src/riscv/run-tests.py
+++ tests/test-progs/asmtest/src/riscv/run-tests.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 # Copyright (c) 2018, Cornell University
 # All rights reserved.
@@ -36,12 +36,10 @@
 import os
 import sys
 import argparse
+import signal
 import subprocess
 
 from multiprocessing import Pool
-
-import clusterjob
-from clusterjob import *
 
 #-------------------------------------------------------------------------
 # utility function to run a process
@@ -58,6 +56,18 @@ def execute(cmd):
 
 # general options
 parser = argparse.ArgumentParser(description='run RISC-V assembly tests')
+parser.add_argument('--test',
+                    help = 'execute only this test',
+                    type = str,
+                    default = None)
+parser.add_argument('--cpu-type',
+                    help = 'use only the given CPU model',
+                    type = str,
+                    default = None)
+parser.add_argument('--debug-flags',
+                    help = 'the debug flags to pass to gem5',
+                    type = str,
+                    default = None)
 parser.add_argument('--max-tick',
                     help = 'maximum simulated tick',
                     type = int,
@@ -82,60 +92,120 @@ test_summary_out = './test-summary.out'
 #-------------------------------------------------------------------------
 gem5_dir = os.path.abspath('../../../../../')
 gem5_bin = os.path.join(gem5_dir, 'build', 'RISCV', 'gem5.opt')
-config = os.path.join(gem5_dir, 'configs', 'example', 'se.py')
 
 # list of CPU models to be tested
-cpu_models = ['AtomicSimpleCPU',
-              'TimingSimpleCPU',
-              'MinorCPU',
-              'DerivO3CPU']
+cpu_models = [
+  'AtomicSimpleCPU',
+  'TimingSimpleCPU',
+  'MinorCPU',
+  'DerivO3CPU'
+]
 
-# get a list of test binaries in the given directory
-tests = []
-for line in execute("ls %s/*" % (test_dir)).splitlines():
-  if line:
-    tests.append(line.split('/')[-1])
+def run_test(fargs):
+  # workaround for python bug: prevent child processes from getting SIGINT
+  # and handle it only in the parent process (see try-except below)
+  # taken from: https://noswap.com/blog/python-multiprocessing-keyboardinterrupt
+  signal.signal(signal.SIGINT, signal.SIG_IGN)
 
-# total number of tests to run
-n_tests = len(tests) * len(cpu_models)
+  test = fargs[0]
+  model = fargs[1]
+  test_name = test + '-' + model
+
+  out_dir = "m5out/" + test_name
+  if not os.path.isdir(out_dir):
+    os.mkdir(out_dir)
+
+  if "-ps-" in test:
+    config = os.path.join(gem5_dir, 'configs', 'example', 'se.py')
+  else:
+    config = os.path.join(gem5_dir, 'configs', 'example', 'fs.py')
+
+  cmd = [
+    gem5_bin,
+    '-d', out_dir,
+    '--listener-mode', 'off',
+  ]
+  if not args.debug_flags is None:
+    cmd += ['--debug-flags', str(args.debug_flags)]
+
+  cmd += [
+    config,
+    '-m', str(args.max_tick),
+    '--cpu-type', model,
+  ]
+
+  if "-ps-" in test:
+    cmd += [
+      '-n', str(args.num_cpus),
+      '--ruby' if args.ruby else '--caches',
+      '-c', test_dir + '/' + test
+    ]
+  else:
+    cmd += [
+      '--caches',
+      '--mem-size', '3072MB',
+      '--kernel', test_dir + '/' + test
+    ]
+
+  out = open(out_dir + "/log.txt", "w")
+  return subprocess.call(cmd, stdout=out, stderr=subprocess.STDOUT)
+
+if not args.test is None:
+  tests = [args.test]
+else:
+  # get a list of test binaries in the given directory
+  tests = []
+  for line in execute("ls %s/*" % (test_dir)).splitlines():
+    if line:
+      tests.append(line.split('/')[-1])
 
 # make a list of jobs
 job_cmds = []
 job_names = []
 for test in tests:
-  for model in cpu_models:
-    test_name = test + '-' + model
+  if not args.cpu_type is None:
+    test_name = test + '-' + args.cpu_type
     job_names.append(test_name)
-    job_cmds.append([gem5_bin,
-                     '-d', 'm5out/' + test_name,
-                     '--listener-mode', 'off',
-                     config,
-                     '-m', str(args.max_tick),
-                     '--cpu-type', model,
-                     '-n', str(args.num_cpus),
-                     '-c', test_dir + '/' + test,
-                     '--ruby' if args.ruby else '--caches',
-                    ])
+    job_cmds.append([test, args.cpu_type])
+  else:
+    for model in cpu_models:
+      test_name = test + '-' + model
+      job_names.append(test_name)
+      job_cmds.append([test, model])
 
 # execute all jobs
-job_pool = Pool(processes = n_tests)
-job_outputs = job_pool.map(subprocess.call, job_cmds)
+job_pool = Pool()
+job_outputs = []
+try:
+  total = len(job_cmds)
+  for r in job_pool.imap(run_test, job_cmds):
+    job_outputs.append(r)
+    print("\rRunning tests... [{}/{}]".format(len(job_outputs), total)),
+    sys.stdout.flush()
+  print("\rRunning tests... [{}/{}]".format(len(job_outputs), total))
+except KeyboardInterrupt:
+  job_pool.terminate()
+  job_pool.join()
 job_pool.close()
 
-# process job outputs
-file = open(test_summary_out, "w")
+if args.test is None:
+  # process job outputs
+  file = open(test_summary_out, "w")
 
-job_outputs = zip(job_names, job_outputs)
-for entry in job_outputs:
-  # a negative return value indicates that the job was terminated
-  # by a signal
-  # a positive return value indicates that the job exited with a return
-  # value
-  if entry[1] < 0:
-    file.write("%-50s failed - signal = %d\n" % (entry[0], -1 * entry[1]))
-  elif entry[1] > 0:
-    file.write("%-50s failed - status = %d\n" % (entry[0], entry[1]))
-  else:
-    file.write("%-50s passed\n" % (entry[0]))
+  job_outputs = zip(job_names, job_outputs)
+  for entry in job_outputs:
+    # a negative return value indicates that the job was terminated
+    # by a signal
+    # a positive return value indicates that the job exited with a return
+    # value
+    if entry[1] < 0:
+      file.write("%-50s failed - signal = %d\n" % (entry[0], -1 * entry[1]))
+    elif entry[1] > 0:
+      file.write("%-50s failed - status = %d\n" % (entry[0], entry[1]))
+    else:
+      file.write("%-50s passed\n" % (entry[0]))
 
-file.close()
+  file.close()
+else:
+  print("\nResults:")
+  print(zip(job_names, job_outputs))
