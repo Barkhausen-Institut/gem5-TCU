ac779b42e8d317517ba1df88464c5b58e1f6c1ea
Nils Asmussen <nils@os.inf.tu-dresden.de>
2020-04-16 17:11:12 +0200

    sim: warn about multiple power state changes in same tick just once.
    We don't care about that and it's annoying.

--- src/sim/clocked_object.cc
+++ src/sim/clocked_object.cc
@@ -89,9 +89,9 @@ ClockedObject::pwrState(Enums::PwrState p)
     // Any state change related stats would have been recorded on previous call
     // to the pwrState() function.
     if (prvEvalTick == curTick() && curTick() != 0) {
-       warn("ClockedObject %s: More than one power state change request "\
-             "encountered within the same simulation tick %d",
-             name(), prvEvalTick);
+       warn_once("ClockedObject %s: More than one power state change request "\
+                 "encountered within the same simulation tick %d",
+                 name(), prvEvalTick);
         _currPwrState = p;
         return;
     }
