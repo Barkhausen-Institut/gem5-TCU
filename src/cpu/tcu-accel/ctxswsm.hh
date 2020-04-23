8856b255970142901e827e5dc35ac8c8893d5b3d
Nils Asmussen <nils@os.inf.tu-dresden.de>
2020-04-23 10:42:14 +0200

    TcuAccel: fixed receive buffer address.

--- src/cpu/tcu-accel/ctxswsm.hh
+++ src/cpu/tcu-accel/ctxswsm.hh
@@ -37,7 +37,7 @@ class AccelCtxSwSM
 {
     static const uint64_t OUR_VPE   = 0xFFFF;
 
-    static const size_t RBUF_ADDR   = 0x1FF000;
+    static const size_t RBUF_ADDR   = 0x1FF040;
 
     static const unsigned EP_RECV   = 2;
     static const size_t MSG_SIZE    = 64;
