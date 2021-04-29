4f1c530ac042e032adf1fe322e55dff4d113762d
Nils Asmussen <nils@os.inf.tu-dresden.de>
2021-04-29 09:55:10 +0200

    arch-arm: fixed argument registers for aarch32.
    The maximum number of arguments to pseudo operations are currently 6.
    Since on aarch32 every argument has 64-bit, two registers are used per
    argument. For that reason, we need at most 12 registers.

--- src/arch/arm/reg_abi.cc
+++ src/arch/arm/reg_abi.cc
@@ -30,7 +30,11 @@
 namespace ArmISA
 {
 
-const std::vector<int> RegABI32::ArgumentRegs = {0, 1, 2, 3, 4, 5, 6};
-const std::vector<int> RegABI64::ArgumentRegs = {0, 1, 2, 3, 4, 5, 6};
+const std::vector<int> RegABI32::ArgumentRegs = {
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
+};
+const std::vector<int> RegABI64::ArgumentRegs = {
+    0, 1, 2, 3, 4, 5, 6
+};
 
 } // namespace ArmISA
